4ace6d590939d37f9e946a489f10cb09a7fa9f60
Kirill <kirvan130189@gmail.com>
2026-01-20 12:46:09 +0300

    3

--- 01/src/main.tf
+++ 01/src/main.tf
@@ -27,7 +27,7 @@ resource "docker_image" "nginx_image" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "example_${random_password.random_string_FAKE.resulT}"
+  name  = "example_${random_password.random_string.result}"
 
   ports {
     internal = 80
