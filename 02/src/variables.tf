fd63f9725dea1a217c285701763831b6e1ab10a4
Kirill <kirvan130189@gmail.com>
2026-02-08 13:43:53 +0300

    2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -33,6 +33,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2bFomo0QnSKhW6mQNo904PDY4UUlNKyx29LHGn3zgsxPHkfn46k0SkEqvkZX6cuY8HTGSpEwgu8db3BfrYkMpJLdDg8QAyQPbWrdNNT09k6vtldB51j9Kij32/JAnyJp8A6bn+jj/ge6ZboYVQ7v0ZdUIvP67SqGRpQAK+99M0cdJRVap0dzTwVSoGCsBNR1rUtzNj7Yr/C/QRuD0LlIG7A3Kgmt5tH32z0TXRnvBV1oHjS4iG8LzYXKSJqobXyHSwbhoWYYRyeTAHHY29gbU/9B+gfRsLdIPg0Vr35w14FmzuWHBRHIoRbgFHgL0OEGfDU0ynQEtgD2R3uJTNIXj"
   description = "ssh-keygen -t ed25519"
 }
